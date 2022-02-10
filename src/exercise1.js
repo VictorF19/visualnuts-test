36742308a13547290490ca1a37b8ab3fd1b11ca8
Victor F <fernandes.victor@hotmail.com>
2022-02-10 18:35:28 -0300

    feat(exercise1): Initial development of the exercise 1
    Development of the exercise 1 code.
    Creation of the exercise 2 file.

---
+++ src/exercise1.js
@@ -0,0 +1,52 @@
+const validateInput = (targetMaxInteger) => Number.isInteger(targetMaxInteger) && targetMaxInteger >= 1;
+
+function generateListToBePrinted(targetMaxInteger) {
+
+    if (!validateInput(targetMaxInteger)) {
+        console.error('Error: This input is invalid to this function');
+        return;
+    }
+
+    const valuesToBePrinted = [];
+
+    for (i = 1; i <= targetMaxInteger; i++) {
+        let valueToBePrinted = '';
+
+        if (i % 3 !== 0 && i % 5 !== 0) {
+            valueToBePrinted = i;
+        } else {
+
+            if (i % 3 === 0) {
+                valueToBePrinted = 'Visual ';
+            }
+
+            if (i % 5 === 0) {
+                valueToBePrinted += 'Nuts';
+            }
+
+        }
+
+        valuesToBePrinted.push(valueToBePrinted);
+    }
+
+    return valuesToBePrinted;
+};
+
+(function printIntegers(targetMaxInteger) {
+    const valuesToBePrinted = generateListToBePrinted(targetMaxInteger);
+    
+    for(const value of valuesToBePrinted) {
+        console.log(value);
+    }
+
+    return;
+})(100)
+
+// Write or describe an algorithm that prints the whole integer numbers to the console, start
+// from the number 1, and print all numbers going up to the number 100.
+// However, when the number is divisible by 3, do not print the number, but print the word
+// 'Visual'. If the number is divisible by 5, do not print the number, but print 'Nuts'. And for
+// all numbers divisible by both (eg: the number 15) the same, but print 'Visual Nuts'.
+// How will you keep this code safe from bugs? Show how you would guarantee that this code
+// keeps working when developers start making small feature adjustments. (Maybe we would
+// want to print the first 500 numbers, ...).
